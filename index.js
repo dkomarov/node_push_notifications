c2b37f61d3d115e4228c864f219358e4fc23adb8
dkomarov <dkomarov@hawk.iit.edu>
2022-09-03 12:11:44 -0500

    add heroku port

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const publicVapidKey =
   "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
 
-webpush.setVapidDetails(
-  "mailto:test@test.com",
-  publicVapidKey,
-  privateVapidKey
-);
+webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey);
 
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
@@ -32,11 +28,9 @@ app.post("/subscribe", (req, res) => {
   const payload = JSON.stringify({ title: "Push Test" });
 
   // Pass object into sendNotification
-  webpush
-    .sendNotification(subscription, payload)
-    .catch(err => console.error(err));
+  webpush.sendNotification(subscription, payload).catch((err) => console.error(err));
 });
 
-const port = 5000;
+const port = 4000 || process.env.PORT;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
